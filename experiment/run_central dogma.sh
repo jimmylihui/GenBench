ecb03954617fc3ebf719ba91a9f1f6260fbadb3f
Lei Xin <161269757+Ssupercoder@users.noreply.github.com>
2024-09-29 08:50:20 +0800

    Add files via upload

---
+++ experiment/run_central dogma.sh
@@ -0,0 +1,6 @@
+python train.py --model_name DNABERT
+python train.py --model_name DNABERT2
+python train.py --model_name GENA_LM
+python train.py --model_name Caduceus
+python train.py --model_name HyenaDNA
+python train.py --model_name NT
